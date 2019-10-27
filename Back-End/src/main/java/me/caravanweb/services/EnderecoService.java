4c5b35d135f09393df915ae67ff1c4f4ff528c13
Rastrian <luisgustavovaz@live.com>
2019-10-27 19:54:18 -0300

    Missing updates

--- Back-End/src/main/java/me/caravanweb/services/EnderecoService.java
+++ Back-End/src/main/java/me/caravanweb/services/EnderecoService.java
@@ -1,5 +1,21 @@
 package me.caravanweb.services;
 
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+
+import me.caravanweb.DAO.EnderecoDAO;
+
+@Service
 public class EnderecoService {
 
+	private EnderecoDAO repository;
+	
+	public EnderecoService() {
+		try {
+			repository = new EnderecoDAO();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
--- Back-End/src/main/java/me/caravanweb/services/UsuarioService.java
+++ Back-End/src/main/java/me/caravanweb/services/UsuarioService.java
@@ -1,5 +1,21 @@
 package me.caravanweb.services;
 
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+
+import me.caravanweb.DAO.UsuarioDAO;
+
+@Service
 public class UsuarioService {
+	
+	private UsuarioDAO repository;
+	
+	public UsuarioService() {
+		try {
+			repository = new UsuarioDAO();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
