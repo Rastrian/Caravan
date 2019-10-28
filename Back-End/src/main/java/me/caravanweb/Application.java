2406036d9583d9f37d746d7c9d0154587ea345e6
Rastrian <luisgustavovaz@live.com>
2019-10-28 02:04:34 -0300

    Downgrade of Maven
    Downgrade of maven and Sprint parent for compile (pom.xml and deprecated syntax import fix).

--- Back-End/src/main/java/me/caravanweb/Application.java
+++ Back-End/src/main/java/me/caravanweb/Application.java
@@ -2,7 +2,7 @@ package me.caravanweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
