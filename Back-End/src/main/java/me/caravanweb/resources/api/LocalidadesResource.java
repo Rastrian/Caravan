0c5f7c4ecf76490b11bcbb2c582edb6e9470b516
Rastrian <luisgustavovaz@live.com>
2019-10-29 12:15:48 -0300

    Add CrossOrigin Spring flag

--- Back-End/src/main/java/me/caravanweb/resources/api/LocalidadesResource.java
+++ Back-End/src/main/java/me/caravanweb/resources/api/LocalidadesResource.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/api/localidades")
 public class LocalidadesResource {
 	@Autowired
