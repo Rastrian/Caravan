336b61a0fe4f66dfb710268ed895141feacb241d
ThiagoQueirozSilva <50524265+ThiagoQueirozSilva@users.noreply.github.com>
2019-11-06 20:40:46 -0300

    File para o Trabalho de Collections

---
+++ TrabCollections/src/EnderecoDAO.java
@@ -0,0 +1,133 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//import org.springframework.stereotype.Repository;
+
+
+//@Repository
+public class EnderecoDAO implements DAO<Endereco, Integer> {
+	private String filename = "enderecos.bin";
+	private File file = new File(filename);
+	private static List<Endereco> enderecos;
+	private FileOutputStream fos;
+	private ObjectOutputStream outputFile;
+
+	public EnderecoDAO() throws IOException {
+		if (!file.exists()) {
+			fos = new FileOutputStream(file, false);
+			outputFile = new ObjectOutputStream(fos);
+		}
+		enderecos = new ArrayList<Endereco>();
+		readFromFile();
+	}
+
+	@Override
+	public Endereco get(Integer id) {
+		readFromFile();
+		for (Endereco end : enderecos) {
+			if (end.getId() == id) {
+				return end;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean add(Endereco endereco) {
+		try {
+			if(!enderecos.contains(endereco))
+				enderecos.add(endereco);
+			saveToFile();
+			return true;
+		} catch (Exception e) {
+			System.out.println("ERRO ao gravar o endereco '" + endereco.getId() + "' no disco!");
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public void update(Endereco endereco) {
+		int index = enderecos.indexOf(endereco);
+		if (index != -1) {
+			enderecos.set(index, endereco);
+		}
+		saveToFile();
+
+	}
+
+	@Override
+	public void remove(Endereco endereco) {
+		int index = enderecos.indexOf(endereco);
+		if (index != -1) {
+			enderecos.remove(index);
+		}
+		saveToFile();
+
+	}
+
+	private void saveToFile() {
+		try {
+			fos = new FileOutputStream(file, false);
+			outputFile = new ObjectOutputStream(fos);
+			
+			for (Endereco end : enderecos) {
+				outputFile.writeObject(end);
+			}
+			outputFile.flush();
+			readFromFile();
+		} catch (Exception e) {
+			System.out.println("ERRO ao gravar endereco no disco!");
+			e.printStackTrace();
+		}
+	}
+	
+	private List<Endereco> readFromFile() {
+		enderecos = new ArrayList<Endereco>();
+		Endereco endereco = null;
+		try (FileInputStream fis = new FileInputStream(file);
+				ObjectInputStream inputFile = new ObjectInputStream(fis)) {
+			while (fis.available() > 0) {
+				endereco = (Endereco) inputFile.readObject();
+				enderecos.add(endereco);
+			}
+		} catch (Exception e) {
+			System.out.println("ERRO ao gravar produto no disco!");
+			e.printStackTrace();
+		}
+		return enderecos;
+	}
+
+	@Override
+	public List<Endereco> getAll() {
+		return enderecos;
+	}
+	
+	public List<Endereco> getPessoasDeUmaMesmaCidade(String cidade){
+		return enderecos.stream().filter(end -> end.getCidade().equals(cidade)).collect(Collectors.toList());
+	}
+	
+	public int count() {
+		return readFromFile().size();
+	}
+
+	private void close() throws IOException {
+		outputFile.close();
+		fos.close();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		this.close();
+	}
+
+}
---
+++ TrabCollections/src/Usuario.java
@@ -0,0 +1,97 @@
+
+
+import java.io.Serializable;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//@Entity
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Id
+	private Integer id;
+	//@Column(nullable=false) 
+	private String nome;
+	//@Column(nullable=false,unique=true) 
+	private String email;
+	//@Column(nullable=false) 
+	private String senha;
+	
+	private Endereco end;
+	private boolean Turista;
+	private boolean admin;
+	private String imgUrl = "./assets/img/default-user.png";
+	
+	public Usuario() {
+	}
+	
+	//@JsonIgnore
+	public Usuario(String nome,String email,String senha) {
+		super();
+		setNome(nome);
+		setEmail(email);
+		setSenha(senha);
+		this.Turista=true;
+		this.admin = false;
+		this.imgUrl = "./assets/img/default-user.png";
+	}
+	
+	public void seturlImg(String url) {
+		this.imgUrl = url;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public boolean isTurista() {
+		return Turista;
+	}
+	public void setTurista(boolean turista) {
+		Turista = turista;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	public boolean isAdmin() {
+		return this.admin;
+	}
+	
+	public Endereco getEndereco() {
+		return end;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return this.id.equals(((Usuario) obj).getId());
+	}
+}
