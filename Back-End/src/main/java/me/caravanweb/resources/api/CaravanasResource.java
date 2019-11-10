095ccb25f1229e03a77f7032fcf26ee88b272d1a
Rastrian <luisgustavovaz@live.com>
2019-11-10 17:04:54 -0300

    CORS Controller & Fixed Register of Caravana

--- Back-End/src/main/java/me/caravanweb/resources/api/CaravanasResource.java
+++ Back-End/src/main/java/me/caravanweb/resources/api/CaravanasResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +47,9 @@ public class CaravanasResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping
+	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@CrossOrigin
+	@PostMapping(value = "/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public @ResponseBody ResponseEntity<String> create(@RequestBody Caravanas c){
 		String body =  service.add(c);
